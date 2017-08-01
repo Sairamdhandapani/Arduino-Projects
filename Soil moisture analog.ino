08c199fb65e64b760cf84d9b2769f8bd6ea00b70
Sairam <silentsairam@gmail.com>
2017-08-01 11:48:18 +0530

    Add files via upload

---
+++ Soil moisture analog.ino
@@ -0,0 +1,31 @@
+int sensor_pin = A0;
+//int output_value ;
+void setup()
+{
+
+  Serial.begin(9600);
+  Serial.println("Reading From the Sensor ...");
+  delay(2000);
+
+}
+
+void loop()
+{
+  int value = analogRead(sensor_pin);
+  //value = value / 10;
+  Serial.println(value);
+  if ( value < 400)
+  {
+    Serial.println("turn off pump");
+    digitalWrite (13, LOW);
+    delay(2000);
+  }
+  else
+  {
+    Serial.println("turn on pump");
+    digitalWrite (13, HIGH);
+    delay(2000);
+  }
+}
+
+
